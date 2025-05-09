b8c17f01263b3917d45239f6df66a7c5b9574e5b
Star Is You💫 <vewarisking575@gmail.com>
2025-05-09 11:46:22 +0700

    Update Bb-SkinChanger.lua

--- Bb-SkinChanger.lua
+++ Bb-SkinChanger.lua
@@ -3,4 +3,4 @@
 discord.gg/starxhub
 
 --]]
-loadstring(game:HttpGet("https://github.com/CodeE4X-dev/Star-X/releases/download/rblx/e209f6bfdcc066f6.lua"))()
+loadstring(game:HttpGet("https://github.com/CodeE4X-dev/Star-X/releases/download/rblx/SkinChangerBB.lua"))()
