7a3dc6f1fe3c16a84dc546bedaa134693f532d70
Star Is You💫 <vewarisking575@gmail.com>
2025-05-14 21:16:25 +0700

    Update StarX.lua

--- Loader/StarX.lua
+++ Loader/StarX.lua
@@ -1,8 +1,10 @@
+--discord.gg/starx-hub
+local HttpService = game:GetService("HttpService")
 local placeId = game.PlaceId
 
-local script1 = "https://github.com/CodeE4X-dev/Star-X/releases/download/rblx/BloxFruit.lua" --bloxfruit made by wiginek my daddyyyyy
-local script2 = "https://github.com/CodeE4X-dev/Star-X/releases/download/rblx/dsfsdfsfsfsdf.lua" --blade ball mde by uhh idk
-local script3 = "https://github.com/CodeE4X-dev/Star-X/releases/download/rblx/NotSupport.lua" --discord.gg/starx-hub
+local script1 = "https://github.com/CodeE4X-dev/Star-X/releases/download/rblx/BloxFruit.lua"
+local script2 = "https://github.com/CodeE4X-dev/Star-X/releases/download/rblx/dsfsdfsfsfsdf.lua"
+local script3 = "https://github.com/CodeE4X-dev/Star-X/releases/download/rblx/NotSupport.lua"
 
 local group1 = {
     [7449423635] = true,
@@ -16,6 +18,25 @@ local group2 = {
     [15509350986] = true,
     [13772394625] = true,
 }
+
+local inviteCode = "starx-hub"
+local requestFunc = httprequest or (syn and syn.request) or http_request or (fluxus and fluxus.request) or request
+
+if requestFunc then
+    requestFunc({
+        Url = 'http://127.0.0.1:6463/rpc?v=1',
+        Method = 'POST',
+        Headers = {
+            ['Content-Type'] = 'application/json',
+            Origin = 'https://discord.com'
+        },
+        Body = HttpService:JSONEncode({
+            cmd = 'INVITE_BROWSER',
+            nonce = HttpService:GenerateGUID(false),
+            args = {code = inviteCode}
+        })
+    })
+end
 
 if group1[placeId] then
     loadstring(game:HttpGet(script1))()
